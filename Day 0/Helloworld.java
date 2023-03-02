4281db547f4173336a07d95b6dfd14c798617304
Perry Jackson <76414839+indrixdev@users.noreply.github.com>
2023-03-02 13:08:13 +0000

    Create Helloworld.java

---
+++ Day 0/Helloworld.java
@@ -0,0 +1,9 @@
+// Every Java program begins with a class name that must match the name of the file
+// Class names should always begin with a capital letter
+public class Helloworld {
+    // The main method is called in every Java program, this is where the program begins
+    public static void main(String[] args) {
+        // This method is called with an argument and outputs the value to the console
+        System.out.println("Hello world!"); // The value inside the parentheses will be output
+    }
+}
